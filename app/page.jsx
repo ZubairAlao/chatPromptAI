9b71721cbcbce758d87bb84a9f990c93ff5fb0f5
Alao Zubair Lanre <alaozubair@gmail.com>
2024-04-27 18:04:10 +0100

    first chatAIPrompts

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ function Home() {
         <span className="orange_gradient text-center"> AI-Powered Prompts</span>
       </h1>
       <p className="desc text-center">
-        Promptopia is an open-source AI Prompting tool for modern world to discover, create and share creative prompts.
+        chatPromptAI is an open-source AI Prompting tool for modern world to discover, create and share creative prompts.
       </p>
 
       <Feed />
